7f6907f990d23e09237b991027f0fedfbf8f1287
blrk <blradhakrishnan@gmail.com>
2022-02-10 15:01:07 +0530

    Add files via upload

---
+++ intro_class.py
@@ -0,0 +1,82 @@
+# demo of print statment
+print('welcome CE batch-5') # used single quote
+print("CSE department") # used doulbe quote 
+#print("KITS")
+
+# using variables 
+
+a = 10
+b = "Jerin"
+
+print(a)
+print(b)
+
+print("value of a is:", a)
+print("Name is:", b)
+
+myname = "rk"
+my_name = "rk"
+_my_name = "rk"
+MYNAME = "rk"
+myname1 = "rk"
+#1myname = "rk"
+MyName = "rk"
+*myname = "rk"
+my-name = "rk"
+
+# assigning multiple values 
+
+a, b, c = "Jobin", "samuel", "Jerin"
+print(a)
+print(b)
+print(c)
+
+# single value to multiple variable 
+d = e = f = "Joshua"
+print(d)
+print(e)
+print(f)
+
+# using differnt print 
+print("His name is" + d)
+print("His name is " + d)
+
+print(d + a)
+
+x = 5
+y = 2
+print(x+y)
+
+name = input("Give your name")
+
+print(name)
+
+num1 = input("enter a number:")
+
+print(num1)
+
+# identify the type 
+print(type(num1))
+print(type(name))
+print(type(x))
+
+no1 = input("Give the first no:")
+no2 = input("Give the second no:")
+print(no1 + no2)
+
+# data type conversion
+no1 = int(input("Give the first no:"))
+no2 = int(input("Give the second no:"))
+print(no1 + no2)
+
+Question: Write a test a program that accepts the 
+user's name (as string) and age (as number) using 
+input. The program should output a sentence contianing
+
+
+
+
+
+
+
+
