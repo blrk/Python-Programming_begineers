2ad1786521f4ddcfb4358f14579fa1260c11e598
blrk <blradhakrishnan@gmail.com>
2022-02-11 16:09:54 +0530

    Add files via upload

---
+++ Lecture1.py
@@ -0,0 +1,110 @@
+# strings
+
+print('') # empty string
+print("")
+print(" ")
+
+# escape sequence
+print("""I am using print function to print \n something on the console""")
+print("""I am using print function to print \t something on the console""")
+
+print("I am using print function to print \\ something on the console")
+
+print("I am using print function to print today\'s something on the console")
+
+str1 = "hi " + "Ceomputer Enginerring" + " students"
+print(str1)
+
+print(" " * 5 + "python")
+
+# multi line comment
+"""
+this program is develped bny RK
+dept of CSE
+KITS
+"""
+ 
+# -2174483648 to 2174483648
+
+print(2174483648 ** 100)
+
+avg_mark = 99.987
+
+print(avg_mark)
+
+# chacter set
+ord('a')
+chr(97)
+
+# Arithmatic 
+
+x = 10
+y = 3
+
+print("sum:", x + y)
+print("diff:", x - y)
+print("mul:", x * y)
+print("div:", x / y)
+print("quo:", x // y)
+print("Mod:", x % y)
+print("Expon:", x ** y)
+print("Negation:", -x)
+
+# mixed mode arithmetic
+print(3.14 * 3 ** 2)
+
+print(round(avg_mark))
+print(int(avg_mark))
+
+# functions
+
+# function defnition 
+def sum(a, b): # a, b are the parameters 
+    r = a + b;
+    return(r) # retrun the value value to the caller
+
+
+print("sum is :", sum(5,7)) # sum() --> function call 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
